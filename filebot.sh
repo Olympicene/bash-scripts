3002bbd52f95308d08f3c414627d58f27f14dce4
Olympicene <kevinjcord@gmail.com>
2023-01-07 22:20:45 -0600

    fixed script for permissions

--- filebot.sh
+++ filebot.sh
@@ -10,15 +10,15 @@ echo "%L:" $2 >> $output
 
 if [ $2 == 'Anime' ]
 then
-	filebot -rename -r "$1" --db AniDB -non-strict --action duplicate --output /mnt/nfs_share/ --format "{ db.TheTVDB.plex }" >> $output
+	filebot -rename -r "$1" --db AniDB -non-strict --action duplicate --output /mnt/nfs_share/ --format "{ db.TheTVDB.plex }" -exec chmod -R 777 "{output}" >> $output
 
 elif [ $2 == 'Movies' ]
 then
-	filebot -rename -r "$1" --db TheMovieDB -non-strict --action duplicate --output /mnt/nfs_share/ --format "{plex}" >> $output
+	filebot -rename -r "$1" --db TheMovieDB -non-strict --action duplicate --output /mnt/nfs_share/ --format "{plex}" -exec chmod -R 777 "{output}" >> $output
 
 elif [ $2 == 'TV Shows' ]
 then
-	filebot -rename -r "$1" --db TheMovieDB::TV -non-strict --action duplicate --output /mnt/nfs_share/ --format "{plex}" >> $output
+	filebot -rename -r "$1" --db TheMovieDB::TV -non-strict --action duplicate --output /mnt/nfs_share/ --format "{plex}" -exec chmod -R 777 "{output}" >> $output
 
 else
 	echo "Uncategorized File - doing nothing" >> $output
@@ -27,8 +27,3 @@ fi
 
 echo '--------------------------------------------------------------------------------------' >> $output
 echo >> $output
-
-
-
-# old code
-# filebot -rename -r "$1" --db TheMovieDB::TV -non-strict --action duplicate --output /mnt/nfs_share/ --format "{plex}" -exec chmod -R 777 "{output}" | tee -a $output
